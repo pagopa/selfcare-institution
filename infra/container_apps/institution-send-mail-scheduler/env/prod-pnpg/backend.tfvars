a8e1f0b43c393b5df1356537888446331cbc7b99
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-07-05 13:35:50 +0200

    [SELC-5162] Removed pnpg env and reference, added prefix in locals.tf

--- infra/container_apps/institution-send-mail-scheduler/env/prod-pnpg/backend.tfvars
+++
@@ -1,4 +0,0 @@
-resource_group_name  = "terraform-state-rg"
-storage_account_name = "tfappprodselfcare"
-container_name       = "terraform-state"
-key                  = "selfcare-institution-send-mail-scheduler.institution-pnpg-app.tfstate"
--- infra/container_apps/institution-send-mail-scheduler/locals.tf
+++ infra/container_apps/institution-send-mail-scheduler/locals.tf
@@ -1,12 +1,12 @@
 locals {
-  pnpg_suffix = var.is_pnpg == true ? "-pnpg" : ""
+  prefix            = "selc"
   project     = "selc-${var.env_short}"
 
-  container_app_environment_name = "${local.project}${local.pnpg_suffix}-${var.cae_name}"
+  container_app_environment_name = "${local.project}-${var.cae_name}"
   ca_resource_group_name         = "${local.project}-container-app${var.suffix_increment}-rg"
 
   mongo_db = {
-    mongodb_rg_name               = "${local.prefix}-${var.env_short}${local.pnpg_suffix_mongo}-cosmosdb-mongodb-rg",
-    cosmosdb_account_mongodb_name = "${local.prefix}-${var.env_short}${local.pnpg_suffix_mongo}-cosmosdb-mongodb-account"
+    mongodb_rg_name               = "${local.prefix}-${var.env_short}-cosmosdb-mongodb-rg",
+    cosmosdb_account_mongodb_name = "${local.prefix}-${var.env_short}-cosmosdb-mongodb-account"
   }
 }
