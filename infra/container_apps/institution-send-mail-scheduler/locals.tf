ad2dfcf26155f2e4940dab54b97bc69c82a8012e
flaminiaScarciofolo <flaminia.scarciofolo@nttdata.com>
2024-07-05 13:03:30 +0200

    [SELC-5162] Added local values

--- infra/container_apps/institution-send-mail-scheduler/locals.tf
+++ infra/container_apps/institution-send-mail-scheduler/locals.tf
@@ -4,4 +4,9 @@ locals {
 
   container_app_environment_name = "${local.project}${local.pnpg_suffix}-${var.cae_name}"
   ca_resource_group_name         = "${local.project}-container-app${var.suffix_increment}-rg"
+
+  mongo_db = {
+    mongodb_rg_name               = "${local.prefix}-${var.env_short}${local.pnpg_suffix_mongo}-cosmosdb-mongodb-rg",
+    cosmosdb_account_mongodb_name = "${local.prefix}-${var.env_short}${local.pnpg_suffix_mongo}-cosmosdb-mongodb-account"
+  }
 }
