da852852f92e3c3209632597588d9c83374bccc6
manuraf <manuel.rafeli@pagopa.it>
2024-07-05 14:14:41 +0200

    change name upper case

--- infra/container_apps/institution-send-mail-scheduler/mongo_db.tf
+++ infra/container_apps/institution-send-mail-scheduler/mongo_db.tf
@@ -1,7 +1,7 @@
 module "mongodb_collection_pec_notifications" {
   source = "git::https://github.com/pagopa/terraform-azurerm-v3.git//cosmosdb_mongodb_collection?ref=v7.39.0"
 
-  name                = "pecNotifications"
+  name                = "PecNotification"
   resource_group_name = local.mongo_db.mongodb_rg_name
 
   cosmosdb_mongo_account_name  = local.mongo_db.cosmosdb_account_mongodb_name
