bfee7bf500c807797f40db912001f31cf57d034b
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2025-03-24 09:09:13 +0100

    [SELC-6497] New MailNotification model in institution-send-mail-scheduler

--- apps/institution-send-mail-scheduler/src/main/java/it/pagopa/selfcare/institution/config/ProductConfig.java
+++ apps/institution-send-mail-scheduler/src/main/java/it/pagopa/selfcare/institution/config/ProductConfig.java
@@ -1,11 +1,10 @@
 package it.pagopa.selfcare.institution.config;
 
 import io.smallrye.config.ConfigMapping;
-import lombok.Data;
-
-import java.util.Map;
 
 @ConfigMapping(prefix = "institution-send-mail.scheduler")
 public interface ProductConfig {
-    Map<String, Integer> products();
+
+    Integer pecNotificationFrequency();
+
 }
--- apps/institution-send-mail-scheduler/src/main/java/it/pagopa/selfcare/institution/service/InstitutionSendMailScheduledServiceImpl.java
+++ apps/institution-send-mail-scheduler/src/main/java/it/pagopa/selfcare/institution/service/InstitutionSendMailScheduledServiceImpl.java
@@ -5,8 +5,7 @@ import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
-import it.pagopa.selfcare.institution.config.ProductConfig;
-import it.pagopa.selfcare.institution.entity.PecNotification;
+import it.pagopa.selfcare.institution.entity.MailNotification;
 import it.pagopa.selfcare.product.entity.Product;
 import it.pagopa.selfcare.product.service.ProductService;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -14,6 +13,8 @@ import jakarta.inject.Inject;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.openapi.quarkus.selfcare_user_json.api.InstitutionApi;
+import org.openapi.quarkus.selfcare_user_json.model.OnboardedProductResponse;
+import org.openapi.quarkus.selfcare_user_json.model.OnboardedProductState;
 import org.openapi.quarkus.selfcare_user_json.model.UserInstitutionResponse;
 
 import java.time.Duration;
@@ -23,18 +24,17 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class InstitutionSendMailScheduledServiceImpl implements InstitutionSendMailScheduledService {
 
     private static final Logger log = LoggerFactory.getLogger(InstitutionSendMailScheduledServiceImpl.class);
 
-    private static final String PRODUCT_PLACEHOLDER = "nome_prodotto";
-    private static final String USER_COUNT_PLACEHOLDER = "numero_utenti";
-
     private static final String ID_INSTITUTION_PLACEHOLDER = "id_institution";
+    private static final String ADDED_USERS_LIST_PLACEHOLDER = "added_users_list";
+    private static final String REMOVED_USERS_LIST_PLACEHOLDER = "removed_users_list";
 
-    private static final String ID_PRODUCT_PLACEHOLDER = "id_prodotto";
     private final MailServiceImpl mailService;
     private final String templateMail;
     private final String templateMailFirstNotification;
@@ -43,7 +43,7 @@ public class InstitutionSendMailScheduledServiceImpl implements InstitutionSendM
     private final Integer querySize;
 
     private final boolean sendAllNotification;
-    private final ProductConfig productConfig;
+    private final Integer pecNotificationFrequency;
 
     @RestClient
     @Inject
@@ -56,7 +56,7 @@ public class InstitutionSendMailScheduledServiceImpl implements InstitutionSendM
                                                    ProductService productService,
                                                    @ConfigProperty(name = "institution-send-mail.notification-query-size") Integer querySize,
                                                    @ConfigProperty(name = "institution-send-mail.notification-send-all") boolean sendAllNotification,
-                                                   ProductConfig productConfig) {
+                                                   @ConfigProperty(name = "institution-send-mail.scheduler.pec-notification-frequency") Integer pecNotificationFrequency) {
         this.mailService = mailService;
         this.templateMail = templateMail;
         this.templateMailFirstNotification = templateMailFirstNotification;
@@ -64,127 +64,121 @@ public class InstitutionSendMailScheduledServiceImpl implements InstitutionSendM
         this.startDate = startDate;
         this.querySize = querySize;
         this.sendAllNotification = sendAllNotification;
-        this.productConfig = productConfig;
+        this.pecNotificationFrequency = pecNotificationFrequency;
     }
 
     @Override
-    public Uni<Void> retrieveInstitutionFromPecNotificationAndSendMail() {
-
-        return Multi.createFrom().iterable(productConfig.products().keySet())
-                .onItem().transformToUniAndMerge(productId ->
-                        retrieveFilteredAndPaginatedPecNotification(calculateModuleDayOfTheEpoch(productConfig.products().get(productId)), productId))
-                .toUni().replaceWithVoid();
-    }
-
-    private Uni<Void> retrieveFilteredAndPaginatedPecNotification(Long moduleDayOfTheEpoch, String productId) {
+    public Uni<Void> retrieveInstitutionFromMailNotificationAndSendMail() {
+        final long moduleDayOfTheEpoch = calculateModuleDayOfTheEpoch(pecNotificationFrequency);
         return Multi.createBy().repeating()
                 .uni(AtomicInteger::new,
-                        currentPage -> runQueryAndSendNotification(moduleDayOfTheEpoch, currentPage.getAndIncrement(), productId))
+                        currentPage -> runQueryAndSendNotification(moduleDayOfTheEpoch, currentPage.getAndIncrement()))
                 .withDelay(Duration.ofSeconds(30))
                 .until(Boolean.FALSE::equals)
                 .collect()
                 .asList()
                 .replaceWith(Uni.createFrom().voidItem());
-
     }
 
-    public Uni<Boolean> runQueryAndSendNotification(Long moduleDayOfTheEpoch, int page, String productId) {
-        var pecNotificationPage = PecNotification.find(PecNotification.Fields.moduleDayOfTheEpoch.name() + "=?1 AND "
-                        + PecNotification.Fields.productId.name() + "=?2", moduleDayOfTheEpoch, productId)
+    public Uni<Boolean> runQueryAndSendNotification(Long moduleDayOfTheEpoch, int page) {
+        var mailNotificationPage = MailNotification.find(MailNotification.FIELD_MODULE_DAY_OF_THE_EPOCH + "=?1", moduleDayOfTheEpoch)
                 .page(page, querySize);
 
         Uni<List<ReactivePanacheMongoEntityBase>> uniResultEntity = sendAllNotification
-                ? pecNotificationPage.list()
-                : pecNotificationPage.firstResult()
+                ? mailNotificationPage.list()
+                : mailNotificationPage.firstResult()
                     .onItem().ifNotNull().transform(List::of)
                     .onItem().ifNull().continueWith(ArrayList::new);
 
         return uniResultEntity
-                .onItem().invoke(pecNotifications -> log.info(String.format("[%s, moduleDayOfTheEpoch=%d] Page %d processed, iteration has been found %d pec notifications", productId, moduleDayOfTheEpoch, page, pecNotifications.size())))
-                .onItem().transformToUni(this::retrievePecNotificationListAndSendMail)
-                .onItem().invoke(mailSize -> log.info(String.format("[%s, moduleDayOfTheEpoch=%d] Page %d processed, iteration has been send %d mails", productId, moduleDayOfTheEpoch, page, mailSize)))
-                .replaceWith(pecNotificationPage.hasNextPage())
-                .onFailure().invoke(throwable -> log.error(String.format("[%s, moduleDayOfTheEpoch=%d] Error during send mail page %d processed, error=%s", productId, moduleDayOfTheEpoch, page, throwable.getMessage())));
-
+                .onItem().invoke(mailNotifications -> log.info(String.format("[moduleDayOfTheEpoch=%d] Page %d processed, iteration has been found %d mail notifications", moduleDayOfTheEpoch, page, mailNotifications.size())))
+                .onItem().transformToUni(this::retrieveMailNotificationListAndSendMail)
+                .onItem().invoke(mailSize -> log.info(String.format("[moduleDayOfTheEpoch=%d] Page %d processed, iteration has been send %d mails", moduleDayOfTheEpoch, page, mailSize)))
+                .replaceWith(mailNotificationPage.hasNextPage())
+                .onFailure().invoke(throwable -> log.error(String.format("[moduleDayOfTheEpoch=%d] Error during send mail page %d processed, error=%s", moduleDayOfTheEpoch, page, throwable.getMessage())));
     }
 
-    private Uni<Integer> retrievePecNotificationListAndSendMail(List<ReactivePanacheMongoEntityBase> query) {
+    private Uni<Integer> retrieveMailNotificationListAndSendMail(List<ReactivePanacheMongoEntityBase> query) {
         return Multi.createFrom().iterable(query)
-                .onItem().transform(entityBase -> (PecNotification) entityBase)
+                .onItem().transform(entityBase -> (MailNotification) entityBase)
                 .onItem().transformToUniAndMerge(this::constructAndSendMail)
                 .onFailure().invoke(throwable -> log.error("Error during send scheduled mail", throwable))
+                .filter(isMailSent -> isMailSent)
                 .collect().asList()
                 .map(List::size);
     }
 
-    private Uni<Void> constructAndSendMail(PecNotification pecNotification) {
+    private Uni<Boolean> constructAndSendMail(MailNotification mailNotification) {
+        long dayDifference = ChronoUnit.DAYS.between(mailNotification.getCreatedAt(), Instant.now());
 
-        long dayDifference = ChronoUnit.DAYS.between(pecNotification.getCreatedAt(), Instant.now());
         //Onboarding happened today, we must not send mail
         if(dayDifference <= 0) {
-            return Uni.createFrom().voidItem();
-        }
-        if(Objects.isNull(pecNotification.getDigitalAddress())) {
-            log.warn(String.format("Mail did not send for institution %s and product %s because digitalAddress is empty!", pecNotification.getInstitutionId(), pecNotification.getProductId()));
-            return Uni.createFrom().voidItem();
+            log.warn(String.format("Mail did not send for institution %s because onboarding happened today!", mailNotification.getInstitutionId()));
+            return Uni.createFrom().item(false);
         }
 
-        if (sendFirstMail(dayDifference, pecNotification.getProductId())) {
-            Product product = productService.getProduct(pecNotification.getProductId());
-            String productTitle = Optional.ofNullable(product).map(Product::getTitle).orElse(null);
-            Map<String, String> mailParameters = getMailParameters(pecNotification.getInstitutionId(), product, null);
-            return mailService.sendMail(List.of(pecNotification.getDigitalAddress()), templateMailFirstNotification, mailParameters, productTitle)
-                    .onItem().invoke(() -> log.info(String.format("Mail sent for institution %s and product %s", pecNotification.getInstitutionId(), pecNotification.getProductId())))
-                    .onFailure().recoverWithNull();
-        } else {
-            return countNewUsers(pecNotification.getInstitutionId(), pecNotification.getProductId())
-                    .onItem().transformToUni(usersCount -> {
-                        Product product = productService.getProduct(pecNotification.getProductId());
-                        String productTitle = Optional.ofNullable(product).map(Product::getTitle).orElse(null);
-                        Map<String, String> mailParameters = getMailParameters(pecNotification.getInstitutionId(), product, usersCount);
-                        return mailService.sendMail(List.of(pecNotification.getDigitalAddress()), templateMail, mailParameters, productTitle)
-                                .onItem().invoke(() -> log.info(String.format("Mail sent for institution %s and product %s", pecNotification.getInstitutionId(), pecNotification.getProductId())))
-                                .onFailure().recoverWithNull();
-                    });
+        if(Objects.isNull(mailNotification.getDigitalAddress())) {
+            log.warn(String.format("Mail did not send for institution %s because digitalAddress is empty!", mailNotification.getInstitutionId()));
+            return Uni.createFrom().item(false);
         }
+
+        return getInstitutionUsers(mailNotification.getInstitutionId(), mailNotification.getProductIds()).onItem().transformToUni(institutionUsers -> {
+            final Map<String, Integer> addedUsersCount = getAddedUsersCount(institutionUsers);
+            final Map<String, Integer> removedUsersCount = getRemovedUsersCount(institutionUsers);
+            final Map<String, String> mailParameters = getMailParameters(mailNotification.getInstitutionId(), addedUsersCount, removedUsersCount);
+            final String template = sendFirstMail(dayDifference) ? templateMailFirstNotification : templateMail;
+            return mailService.sendMail(List.of(mailNotification.getDigitalAddress()), template, mailParameters, null)
+                    .onItem().invoke(() -> log.info(String.format("Mail sent for institution %s", mailNotification.getInstitutionId())))
+                    .map(v -> true)
+                    .onFailure().recoverWithItem(false);
+        });
     }
 
-    private static Map<String, String> getMailParameters(String institutionId, Product product, Integer users) {
+    private Map<String, String> getMailParameters(String institutionId, Map<String, Integer> addedUsersCount, Map<String, Integer> removedUsersCount) {
         Map<String, String> mailParameters = new HashMap<>();
-        mailParameters.put(PRODUCT_PLACEHOLDER, Optional.ofNullable(product).map(Product::getTitle).orElse(null));
         mailParameters.put(ID_INSTITUTION_PLACEHOLDER, institutionId);
-        mailParameters.put(ID_PRODUCT_PLACEHOLDER, Optional.ofNullable(product).map(Product::getId).orElse(null));
-        if (users != null) {
-            mailParameters.put(USER_COUNT_PLACEHOLDER, users.toString());
-        }
+        final String addedUsersListPlaceholder = addedUsersCount.isEmpty() ? "" : getUsersListPlaceholder("Utenti aggiunti questo mese: ", addedUsersCount);
+        mailParameters.put(ADDED_USERS_LIST_PLACEHOLDER, addedUsersListPlaceholder);
+        final String removedUsersListPlaceholder = removedUsersCount.isEmpty() ? "" : getUsersListPlaceholder("Utenti rimossi questo mese: ", removedUsersCount);
+        mailParameters.put(REMOVED_USERS_LIST_PLACEHOLDER, removedUsersListPlaceholder);
         return mailParameters;
     }
 
-    private Uni<Integer> countNewUsers(String institutionId, String productId) {
-        return userInstitutionApi.institutionsInstitutionIdUserInstitutionsGet(
+    private String getUsersListPlaceholder(String prefix, Map<String, Integer> usersCount) {
+        return String.format("%s<ul>%s</ul>", prefix, String.join("", usersCount.entrySet().stream()
+                .map(e -> String.format("<li><strong>%d</strong> utenti per il prodotto <strong>%s</strong></li>",
+                        e.getValue(), Optional.ofNullable(productService.getProduct(e.getKey())).map(Product::getTitle).orElse("")))
+                .toList()));
+    }
+
+    private Map<String, Integer> getAddedUsersCount(List<UserInstitutionResponse> userInstitutionResponses) {
+        final LocalDateTime xDaysAgo = LocalDateTime.now().minusDays(pecNotificationFrequency);
+        return userInstitutionResponses.stream()
+                .flatMap(u -> u.getProducts().stream())
+                .filter(p -> (OnboardedProductState.ACTIVE.equals(p.getStatus()) || OnboardedProductState.SUSPENDED.equals(p.getStatus())) && p.getCreatedAt() != null && p.getCreatedAt().isAfter(xDaysAgo))
+                .collect(Collectors.groupingBy(OnboardedProductResponse::getProductId, Collectors.summingInt(p -> 1)));
+    }
+
+    private Map<String, Integer> getRemovedUsersCount(List<UserInstitutionResponse> userInstitutionResponses) {
+        final LocalDateTime xDaysAgo = LocalDateTime.now().minusDays(pecNotificationFrequency);
+        return userInstitutionResponses.stream()
+                .flatMap(u -> u.getProducts().stream())
+                .filter(p -> OnboardedProductState.DELETED.equals(p.getStatus()) && p.getUpdatedAt() != null && p.getUpdatedAt().isAfter(xDaysAgo))
+                .collect(Collectors.groupingBy(OnboardedProductResponse::getProductId, Collectors.summingInt(p -> 1)));
+    }
+
+    private Uni<List<UserInstitutionResponse>> getInstitutionUsers(String institutionId, List<String> productIds) {
+        return Optional.ofNullable(productIds)
+                .filter(products -> !products.isEmpty())
+                .map(products -> userInstitutionApi.institutionsInstitutionIdUserInstitutionsGet(
                         institutionId,
                         null,
-                        List.of(productId),
+                        products,
                         null,
                         null,
-                        null)
-                .onItem().transform(userInstitutionResponses -> filterOnCreatedAt(userInstitutionResponses, productId))
-                .onItem().transformToUni(userInstitutionResponses -> Uni.createFrom().item(userInstitutionResponses.size()));
-    }
-
-    private List<UserInstitutionResponse> filterOnCreatedAt(List<UserInstitutionResponse> userInstitutionResponses, String productId){
-        LocalDateTime thirtyDaysAgo = LocalDateTime.now().minusDays(30);
-
-       return userInstitutionResponses.stream()
-                .filter(userInstitutionResponse -> productHasValidCreatedAt(productId, userInstitutionResponse, thirtyDaysAgo))
-                .toList();
-    }
-
-    private static boolean productHasValidCreatedAt(String productId, UserInstitutionResponse userInstitutionResponse, LocalDateTime thirtyDaysAgo) {
-        return !userInstitutionResponse.getProducts().stream()
-                .filter(onboardedProductResponse -> onboardedProductResponse.getProductId().equals(productId) &&
-                        onboardedProductResponse.getCreatedAt().isAfter(thirtyDaysAgo))
-                .toList().isEmpty();
+                        null
+                ))
+                .orElse(Uni.createFrom().item(Collections.emptyList()));
     }
 
     private Long calculateModuleDayOfTheEpoch(Integer sendingFrequency) {
@@ -194,7 +188,7 @@ public class InstitutionSendMailScheduledServiceImpl implements InstitutionSendM
         return dayDifference % sendingFrequency;
     }
 
-    private boolean sendFirstMail(long dayDifference, String productId) {
-        return dayDifference <= productConfig.products().getOrDefault(productId, 0);
+    private boolean sendFirstMail(long dayDifference) {
+        return dayDifference <= pecNotificationFrequency;
     }
 }
