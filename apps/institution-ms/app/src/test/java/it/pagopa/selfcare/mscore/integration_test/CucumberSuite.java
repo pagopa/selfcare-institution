a7547eedd3d17af1b687548b6ea61fb2dd3be6ac
Giulia Tremolada <giulia.tremolada@nttdata.com>
2025-03-25 17:26:51 +0100

    add cucumber-sdk lib

--- apps/institution-ms/app/src/test/java/it/pagopa/selfcare/mscore/integration_test/CucumberSuite.java
+++ apps/institution-ms/app/src/test/java/it/pagopa/selfcare/mscore/integration_test/CucumberSuite.java
@@ -1,17 +1,20 @@
 package it.pagopa.selfcare.mscore.integration_test;
 
 import io.cucumber.spring.CucumberContextConfiguration;
+import io.quarkiverse.cucumber.CucumberOptions;
 import it.pagopa.selfcare.mscore.SelfCareCoreApplication;
-import org.junit.platform.suite.api.*;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.TestPropertySource;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
@@ -20,15 +23,22 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @CucumberContextConfiguration
+@CucumberOptions(
+        glue = {"it.pagopa.selfcare.mscore.integration_test.steps", "it.pagopa.selfcare.cucumber.utils"}
+)
 @SpringBootTest(classes = {SelfCareCoreApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestPropertySource(locations = "classpath:application-test.properties")
-@ExcludeTags({"FeatureDelegation", "FeatureDelegationV2", "FeatureExternal", "FeatureInstitution", "FeatureManagement", "FeatureOnboarding", "FeatureFake"})
+//@ExcludeTags({"FeatureDelegation", "FeatureDelegationV2", "FeatureExternal", "FeatureInstitution", "FeatureManagement", "FeatureOnboarding", "FeatureFake"})
 public class CucumberSuite {
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) throws IOException {
-        final Path filePath = Paths.get("src/test/resources/key/public-key.pub");
-        final String publicKey = String.join("", Files.readAllLines(filePath));
+        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream("key/public-key.pub");
+        if (inputStream == null) {
+            throw new IOException("Public key file not found in classpath");
+        }
+        String publicKey = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
         registry.add("JWT_TOKEN_PUBLIC_KEY", () -> publicKey);
     }
 
