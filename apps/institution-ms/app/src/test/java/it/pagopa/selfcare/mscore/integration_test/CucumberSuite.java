b13b31cb8ff0a94862d7e7ad1baefa8b87eebe19
Giulia Tremolada <giulia.tremolada@nttdata.com>
2025-03-26 15:17:18 +0100

    use ConfigurationParameter instead of CucumberOptions

--- apps/institution-ms/app/src/test/java/it/pagopa/selfcare/mscore/integration_test/CucumberSuite.java
+++ apps/institution-ms/app/src/test/java/it/pagopa/selfcare/mscore/integration_test/CucumberSuite.java
@@ -1,12 +1,8 @@
 package it.pagopa.selfcare.mscore.integration_test;
 
 import io.cucumber.spring.CucumberContextConfiguration;
-import io.quarkiverse.cucumber.CucumberOptions;
 import it.pagopa.selfcare.mscore.SelfCareCoreApplication;
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
@@ -16,6 +12,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
@@ -23,12 +20,10 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @CucumberContextConfiguration
-@CucumberOptions(
-        glue = {"it.pagopa.selfcare.mscore.integration_test.steps", "it.pagopa.selfcare.cucumber.utils"}
-)
 @SpringBootTest(classes = {SelfCareCoreApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestPropertySource(locations = "classpath:application-test.properties")
-//@ExcludeTags({"FeatureDelegation", "FeatureDelegationV2", "FeatureExternal", "FeatureInstitution", "FeatureManagement", "FeatureOnboarding", "FeatureFake"})
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.selfcare.cucumber.utils,it.pagopa.selfcare.mscore.integration_test")
+@ExcludeTags({"FeatureDelegation", "FeatureDelegationV2", "FeatureExternal", "FeatureInstitution", "FeatureManagement", "FeatureOnboarding", "FeatureFake"})
 public class CucumberSuite {
 
     @DynamicPropertySource
