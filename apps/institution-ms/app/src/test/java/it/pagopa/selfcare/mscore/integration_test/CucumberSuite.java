da98088b62c2914a8e1d25bf85292b4d9fc27baa
Pierluigi D'Uffizi <DFFPLG91T09H501X@sogeiesterni.it>
2025-05-22 11:57:31 +0200

    update container configuration, rest assured configuration

--- apps/institution-ms/app/src/test/java/it/pagopa/selfcare/mscore/integration_test/CucumberSuite.java
+++ apps/institution-ms/app/src/test/java/it/pagopa/selfcare/mscore/integration_test/CucumberSuite.java
@@ -29,25 +29,15 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 @CucumberContextConfiguration
-@SpringBootTest(classes = {SelfCareCoreApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(classes = {
+        SelfCareCoreApplication.class,
+        TestContainerConfiguration.class,
+        RestAssuredConfiguration.class
+}, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestPropertySource(locations = "classpath:application-test.properties")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "it.pagopa.selfcare.cucumber.utils,it.pagopa.selfcare.mscore.integration_test")
 @Slf4j
 public class CucumberSuite {
-
-    static {
-        final ComposeContainer composeContainer = new ComposeContainer(new File("src/test/resources/docker-compose.yml"))
-                .withLocalCompose(true).withEnv("GITHUB_TOKEN", System.getenv("GITHUB_TOKEN"))
-                .withLogConsumer("azure-cli", new Slf4jLogConsumer(log))
-                .withLogConsumer("azurite", new Slf4jLogConsumer(log))
-                .withLogConsumer("mongodb", new Slf4jLogConsumer(log))
-                .withLogConsumer("mockserver", new Slf4jLogConsumer(log))
-                .waitingFor("azure-cli", Wait.forLogMessage(".*BLOBSTORAGE INITIALIZED.*\\n", 1));
-        composeContainer.start();
-        Runtime.getRuntime().addShutdownHook(new Thread(composeContainer::stop));
-        RestAssured.port = 8082;
-        RestAssured.baseURI = "http://localhost";
-    }
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) throws IOException {
