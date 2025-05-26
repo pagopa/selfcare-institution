ffb1a77bbe5cc22e3197e9be7d4db34522aeb52b
Pierluigi D'Uffizi <DFFPLG91T09H501X@sogeiesterni.it>
2025-05-26 09:05:19 +0200

    update README.md, update CucumberSuite docker-compose.yml path and property server.port

--- apps/institution-ms/app/src/test/java/it/pagopa/selfcare/mscore/integration_test/CucumberSuite.java
+++ apps/institution-ms/app/src/test/java/it/pagopa/selfcare/mscore/integration_test/CucumberSuite.java
@@ -3,7 +3,6 @@ package it.pagopa.selfcare.mscore.integration_test;
 import io.cucumber.spring.CucumberContextConfiguration;
 import it.pagopa.selfcare.mscore.SelfCareCoreApplication;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
@@ -19,6 +18,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
@@ -34,13 +35,17 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Slf4j
 public class CucumberSuite {
 
-    private static ComposeContainer composeContainer;
+    private static final ComposeContainer composeContainer;
 
-    @BeforeAll
-    static void setup(){
+    static {
         log.info("Starting test containers...");
 
-        composeContainer = new ComposeContainer(new File("../docker-compose.yml"))
+        String dockerComposeFilePath = Paths.get(System.getProperty("user.dir"))
+                .toAbsolutePath()
+                .toString()
+                .replaceFirst("(.*apps).*", "$1/institution-ms/docker-compose.yml");
+
+        composeContainer = new ComposeContainer(new File(dockerComposeFilePath))
                 .withLocalCompose(true)
                 .waitingFor("azure-cli", Wait.forLogMessage(".*BLOBSTORAGE INITIALIZED.*\\n", 1));
         composeContainer.start();
@@ -51,6 +56,15 @@ public class CucumberSuite {
 
     @DynamicPropertySource
     static void setProperties(DynamicPropertyRegistry registry) throws IOException {
+        Properties props = new Properties();
+        try (InputStream input = Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("application-test.properties")) {
+            if (input != null) {
+                props.load(input);
+            }
+        }
+        String serverPort = props.getProperty("server.port", "8082");
+        registry.add("server.port", () -> serverPort);
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
         InputStream inputStream = classLoader.getResourceAsStream("key/public-key.pub");
         if (inputStream == null) {
