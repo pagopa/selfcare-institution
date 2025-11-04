a5c76519bfa011486384c6464e4cd1af0520f7e7
Giulia Tremolada <giulia.tremolada@nttdata.com>
2025-11-04 16:30:45 +0100

    remove unused objects

--- apps/institution-ms/web/src/main/java/it/pagopa/selfcare/mscore/web/model/mapper/OnboardingResourceMapper.java
+++ apps/institution-ms/web/src/main/java/it/pagopa/selfcare/mscore/web/model/mapper/OnboardingResourceMapper.java
@@ -3,35 +3,20 @@ package it.pagopa.selfcare.mscore.web.model.mapper;
 
 import it.pagopa.selfcare.mscore.model.institution.Onboarding;
 import it.pagopa.selfcare.mscore.model.onboarding.OnboardingRequest;
-import it.pagopa.selfcare.mscore.model.onboarding.OnboardingUsersRequest;
 import it.pagopa.selfcare.mscore.web.model.institution.InstitutionOnboardingRequest;
 import it.pagopa.selfcare.mscore.web.model.institution.OnboardingPut;
 import it.pagopa.selfcare.mscore.web.model.institution.OnboardingResponse;
-import it.pagopa.selfcare.mscore.web.model.onboarding.OnboardingInstitutionRequest;
-import it.pagopa.selfcare.mscore.web.model.onboarding.OnboardingInstitutionUsersRequest;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
-import java.time.LocalDateTime;
-import java.time.OffsetDateTime;
-import java.time.ZoneOffset;
 import java.util.Optional;
 
 @Mapper(componentModel = "spring", uses = {InstitutionUpdateMapper.class}, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface OnboardingResourceMapper {
 
     OnboardingResponse toResponse(Onboarding onboarding);
-
-    @Mapping(target = "contractFilePath", source = "contractImported.filePath")
-    @Mapping(target = "contractCreatedAt", source = "contractImported.createdAt")
-    @Mapping(target = "billingRequest", source = "billing")
-    @Mapping(target = "signContract", source = "signContract", qualifiedByName = "mapSignContract")
-    @Mapping(target = "contractActivatedAt", source = "contractImported.activatedAt")
-    OnboardingRequest toOnboardingRequest(OnboardingInstitutionRequest onboardingInstitutionRequest);
-
-    OnboardingUsersRequest toOnboardingUsersRequest(OnboardingInstitutionUsersRequest request);
 
     @Named("mapSignContract")
     default Boolean mapSignContract(Boolean signContract) {
@@ -42,12 +27,6 @@ public interface OnboardingResourceMapper {
     @Mapping(target = "createdAt", source = "activatedAt")
     Onboarding toOnboarding(InstitutionOnboardingRequest onboardingRequest);
 
-    @Named("toOffsetDateTime")
-    default OffsetDateTime toOffsetDateTime(LocalDateTime date) {
-        return Optional.ofNullable(date)
-                .map(ld -> ld.atZone(ZoneOffset.systemDefault()).toOffsetDateTime())
-                .orElse(null);
-    }
     @Mapping(target = "contract", source = "contract.path")
     Onboarding toOnboarding(OnboardingRequest onboardingRequest);
 
