9086c342085b98fb89fed1ff71c4920675f9c93f
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2025-11-10 11:01:21 +0100

    [SELC-7753] Workaround to send authorization header, with the right encoding, only with EventHubRestClient

--- apps/institution-ms/connector/rest/src/main/java/it/pagopa/selfcare/mscore/connector/rest/client/EventHubRestClient.java
+++ apps/institution-ms/connector/rest/src/main/java/it/pagopa/selfcare/mscore/connector/rest/client/EventHubRestClient.java
@@ -6,11 +6,15 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.Map;
 
 @FeignClient(name = "${rest-client.event-hub.serviceCode}", url = "${rest-client.event-hub.base-url}")
 public interface EventHubRestClient {
 
     @PostMapping(value = "/messages", consumes = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<Void> sendMessage(@RequestBody DelegationNotificationToSend notification);
+    ResponseEntity<Void> sendMessage(@RequestBody DelegationNotificationToSend notification,
+                                     @RequestHeader Map<String, String> headers);
 
 }
