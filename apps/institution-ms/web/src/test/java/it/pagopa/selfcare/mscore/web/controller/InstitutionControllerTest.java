a753e0eb811931d33d5764aaac8867efe5c50d84
Giulia Tremolada <giulia.tremolada@nttdata.com>
2025-11-12 09:45:52 +0100

    fix institution controller tests

--- apps/institution-ms/web/src/test/java/it/pagopa/selfcare/mscore/web/controller/InstitutionControllerTest.java
+++ apps/institution-ms/web/src/test/java/it/pagopa/selfcare/mscore/web/controller/InstitutionControllerTest.java
@@ -315,7 +315,7 @@ class InstitutionControllerTest {
         SecurityContext securityContext = Mockito.mock(SecurityContext.class);
         SecurityContextHolder.setContext(securityContext);
         when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getCredentials()).thenReturn("fake-jwt-without-pnpg");
+        when(authentication.getCredentials()).thenReturn(fakeJwt);
 
         when(institutionService.retrieveInstitutionById("42")).thenReturn(createInstitution());
         when(institutionService.getLogo("42")).thenReturn("logoUrl");
@@ -1179,7 +1179,7 @@ class InstitutionControllerTest {
         SecurityContextHolder.setContext(securityContext);
         when(securityContext.getAuthentication()).thenReturn(authentication);
         when(authentication.getPrincipal()).thenReturn(SelfCareUser.builder("id").build());
-        when(authentication.getCredentials()).thenReturn("fake-jwt-without-pnpg");
+        when(authentication.getCredentials()).thenReturn(fakeJwt);
 
         CreatePgInstitutionRequest request = new CreatePgInstitutionRequest();
         request.setTaxId("1234");
