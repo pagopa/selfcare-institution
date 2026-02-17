d227011914e771c5dc35eebedae7363217f9e48a
Gaetano Miglionico <gaetano.miglionico@pagopa.it>
2026-02-17 18:09:06 +0100

    Fixed user registry request interceptor not working in spring boot 3 (#145)

--- apps/institution-ms/connector/rest/src/main/java/it/pagopa/selfcare/mscore/connector/rest/config/UserRegistryRestClientConfig.java
+++ apps/institution-ms/connector/rest/src/main/java/it/pagopa/selfcare/mscore/connector/rest/config/UserRegistryRestClientConfig.java
@@ -1,8 +1,9 @@
 package it.pagopa.selfcare.mscore.connector.rest.config;
 
 import it.pagopa.selfcare.commons.connector.rest.config.RestClientBaseConfig;
+import it.pagopa.selfcare.mscore.connector.rest.interceptor.UserRegistryAuthInterceptor;
 import org.springframework.context.annotation.Import;
 
-@Import({RestClientBaseConfig.class})
+@Import({RestClientBaseConfig.class, UserRegistryAuthInterceptor.class})
 public class UserRegistryRestClientConfig {
 }
